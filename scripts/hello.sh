d486c2c745e20295f1694622e62e836011c20e55
Iryna Dmytriieva <iryna.dmytriieva@namecheap.com>
2025-07-10 21:08:20 +0200

    task 5. optional

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -2,6 +2,7 @@
 
 # Create a directory for the output if do not exist
 mkdir -p output
+mkdir -p logs
 
 # Set current date
 current_date=$(date +%Y%m%d_%H%M%S)
@@ -29,14 +30,22 @@ else
     output_file="log-${current_date}.txt"
 fi
 
+
+
 log_file() {
     echo "LOG_FILE=${output_file}" >> "$GITHUB_ENV"
 }
 log_file
 
+
+
 # Redirect output to a file including errors
 exec > output/$output_file 2>&1
+
 
 greetings
 echo "Current date and time: $current_date"
 echo "Number of files: $(count_files)"
+
+# append to logs/history
+echo "$current_date $output_file" >> logs/history.txt
