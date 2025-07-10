0ce3b3cde4c17e909b5c9c06ae3ffc2d86719686
Iryna Dmytriieva <iryna.dmytriieva@namecheap.com>
2025-07-10 18:43:01 +0200

    task 2. drafts

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -12,19 +12,26 @@ greetings () {
 }
 
 # Count the number of files in the current directory, excluding subdirectories function
-count_files=$(find . -maxdepth 1 -type f | wc -l )
+count_files() {
+    find . -maxdepth 1 -type f | wc -l 
+}
 
-echo "LOG_FILE_COUNT=${count_files}" >> "$GITHUB_ENV"
-
+log_count_files(){
+    echo "LOG_FILE_COUNT=$(count_files)" >> "$GITHUB_ENV"
+}
+log_count_files
 
 # Generate and Export log name so GitHub Actions can read it later
 output_file="log-${current_date}.txt"
 
-echo "LOG_FILE=${output_file}" >> "$GITHUB_ENV"
+log_file() {
+    echo "LOG_FILE=${output_file}" >> "$GITHUB_ENV"
+}
+log_file
 
 # Redirect output to a file including errors
 exec > output/$output_file 2>&1
 
 greetings
 echo "Current date and time: $current_date"
-echo "Number of files: $count_files"
+echo "Number of files: $(count_files)"
